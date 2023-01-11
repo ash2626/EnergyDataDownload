a6dc11956cdfcfaeb76f2a01a69a120e34f9e476
ash26 <Sycam0re7r33!>
2023-01-11 15:34:42 +0000

    updated code to use MYSQL

--- EnergyDownloader.py
+++ EnergyDownloader.py
@@ -4,11 +4,11 @@ from datetime import datetime, timedelta
 import mysql
 from mysql.connector import errorcode
 
-Type = sys.argv[1] if len(sys.argv) > 1 else print("No energy type specified")  # must be "gas" or "electricity"
-MAC = sys.argv[2] if len(sys.argv) > 1 else print("No IHD MAC address specified")
+MAC = sys.argv[6] if len(sys.argv) > 1 else print("No IHD MAC address specified")
+Type = sys.argv[7] if len(sys.argv) > 1 else print("No energy type specified")  # must be "gas" or "electricity"
 section_date = datetime(2021, 10, 1)
-headers = {'Authorization': sys.argv[5]}  # AUTH is my MAC code
-url = "https://consumer-api.data.n3rgy.com/" + sys.argv[6] + "/consumption/1/"
+headers = {'Authorization': sys.argv[6]}  # AUTH is my MAC code
+url = "https://consumer-api.data.n3rgy.com/" + sys.argv[7] + "/consumption/1/"
 
 def createDBconnection():
     try:
@@ -45,20 +45,22 @@ def add90days(ssd):
 cnx = createDBconnection()
 cur = cnx.cursor()
 
-executeSQL(f"CREATE DATABASE IF NOT EXISTS energy;",cur)
-executeSQL(f"CREATE TABLE IF NOT EXISTS gas (id INT auto_increment, date DATE, energy_usage FLOAT, primary key (id) );",cur)
-executeSQL(f"CREATE TABLE IF NOT EXISTS electricity (id INT auto_increment, date DATE, energy_usage FLOAT, primary key (id) );",cur)
+cur.execute(f"CREATE DATABASE IF NOT EXISTS energy;")
+cur.execute(f"CREATE TABLE IF NOT EXISTS gas (id INT auto_increment, timestamp TIMESTAMP, energy_usage FLOAT, primary key (id) );")
+cur.execute(f"CREATE TABLE IF NOT EXISTS electricity (id INT auto_increment, timestamp TIMESTAMP, energy_usage FLOAT, primary key (id) );")
 
 while section_date.date() < datetime.now().date():
     query = '?start=' + section_date.strftime('%Y') + section_date.strftime('%m') + section_date.strftime('%d') + \
             '0000&end=' + add90days(section_date).strftime('%Y') + add90days(section_date).strftime('%m') + \
             add90days(section_date).strftime('%d') + '0000'
     api_url = url + query
+    print(api_url)
     r = requests.get(url=api_url, headers=headers)
 
-    for x in r.json()['values']:
-        #update energy db with each x
-        executeSQL("INSERT INTO TABLE " + Type + " "+ x.values() )
+    for x in r.json()['values']: 
+        data=(x["timestamp"], x["value"])
+        cur.execute("""INSERT INTO """+Type+""" (timestamp, energy_usage) values (%s, %s);""",data)
+        cnx.commit()
 
     section_date = add90days(section_date)
 
